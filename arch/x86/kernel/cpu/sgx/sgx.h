b2ff21a1e48cf396247bbc3574140a9cba4e7a18
Sean Christopherson <sean.j.christopherson@intel.com>
2020-05-15 08:17:08 -0700

    x86/sgx: Introduce RECLAIM_IN_PROGRESS flag for EPC pages
    When a page is being reclaimed from the page pool (sgx_global_lru),
    there is an intermediate stage where a page may have been identified
    as a candidate for reclaiming, but has not yet been reclaimed. If
    another thread decides to drop the same page, it is possible that
    the page could be put back onto the reclaimable list after it has
    been dropped, or it could be dropped in the middle of a reclaim.
    
    In order to prevent a page from being removed from the sgx_epc_lru_lists
    struct in a separate thread by sgx_drop_epc_page(), keep track of
    whether the EPC page is in the middle of being reclaimed with
    the addtion of a RECLAIM_IN_PROGRESS flag, and do not delete the page
    off the LRU in sgx_drop_epc_page() if it has not yet finished being
    reclaimed.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>
    Cc: Sean Christopherson <seanjc@google.com>

--- arch/x86/kernel/cpu/sgx/sgx.h
+++ arch/x86/kernel/cpu/sgx/sgx.h
@@ -31,6 +31,8 @@
 
 /* Pages on free list */
 #define SGX_EPC_PAGE_IS_FREE		BIT(1)
+/* page flag to indicate reclaim is in progress */
+#define SGX_EPC_PAGE_RECLAIM_IN_PROGRESS BIT(2)
 
 struct sgx_epc_page {
 	unsigned int section;
