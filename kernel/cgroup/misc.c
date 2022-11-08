c6b66c171a80b97a340f0c1a1051e7149765179d
Kristen Carlson Accardi <kristen@linux.intel.com>
2022-11-08 09:57:59 -0800

    cgroup/misc: Add per resource callbacks for CSS events
    The misc cgroup controller (subsystem) currently does not perform
    resource type specific action for Cgroups Subsystem State (CSS) events:
    the 'css_alloc' event when a cgroup is created and the 'css_free' event
    when a cgroup is destroyed.
    
    Define callbacks for those events and allow resource providers to
    register the callbacks per resource type as needed. This will be
    utilized later by the EPC misc cgroup support implemented in the SGX
    driver.
    
    This design passes a struct misc_cg into the callbacks. An alternative
    to pass only a struct misc_res was considered for encapsulating how
    misc_cg stores its misc_res array. However, the SGX driver would still
    need to access the misc_res array in the statically defined misc root_cg
    during initialization to set resource specific fields. Therefore, some
    extra getter/setter APIs to abstract such access to the misc_res array
    within the misc_cg struct would be needed. That seems an overkill at the
    moment and is deferred to later improvement when there are more users of
    these callbacks.
    
    Link: https://lore.kernel.org/lkml/op.2kdw36otwjvjmi@hhuan26-mobl.amr.corp.intel.com/
    
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>
    Co-developed-by: Haitao Huang <haitao.huang@linux.intel.com>
    Signed-off-by: Haitao Huang <haitao.huang@linux.intel.com>
    Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Reviewed-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Tested-by: Jarkko Sakkinen <jarkko@kernel.org>
    ---
    V12:
    - Add comments in commit to clarify reason to pass in misc_cg, not
    misc_res. (Kai)
    - Remove unlikely (Kai)
    
    V8:
    - Abstract out _misc_cg_res_free() and _misc_cg_res_alloc() (Jarkko)
    
    V7:
    - Make ops one per resource type and store them in array (Michal)
    - Rename the ops struct to misc_res_ops, and enforce the constraints of required callback
    functions (Jarkko)
    - Moved addition of priv field to patch 4 where it was used first. (Jarkko)
    
    V6:
    - Create ops struct for per resource callbacks (Jarkko)
    - Drop max_write callback (Dave, Michal)
    - Style fixes (Kai)

--- kernel/cgroup/misc.c
+++ kernel/cgroup/misc.c
@@ -39,6 +39,9 @@ static struct misc_cg root_cg;
  */
 static u64 misc_res_capacity[MISC_CG_RES_TYPES];
 
+/* Resource type specific operations */
+static const struct misc_res_ops *misc_res_ops[MISC_CG_RES_TYPES];
+
 /**
  * parent_misc() - Get the parent of the passed misc cgroup.
  * @cgroup: cgroup whose parent needs to be fetched.
@@ -104,6 +107,36 @@ int misc_cg_set_capacity(enum misc_res_type type, u64 capacity)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(misc_cg_set_capacity);
+
+/**
+ * misc_cg_set_ops() - set resource specific operations.
+ * @type: Type of the misc res.
+ * @ops: Operations for the given type.
+ *
+ * Context: Any context.
+ * Return:
+ * * %0 - Successfully registered the operations.
+ * * %-EINVAL - If @type is invalid, or the operations missing any required callbacks.
+ */
+int misc_cg_set_ops(enum misc_res_type type, const struct misc_res_ops *ops)
+{
+	if (!valid_type(type))
+		return -EINVAL;
+
+	if (!ops->alloc) {
+		pr_err("%s: alloc missing\n", __func__);
+		return -EINVAL;
+	}
+
+	if (!ops->free) {
+		pr_err("%s: free missing\n", __func__);
+		return -EINVAL;
+	}
+
+	misc_res_ops[type] = ops;
+	return 0;
+}
+EXPORT_SYMBOL_GPL(misc_cg_set_ops);
 
 /**
  * misc_cg_cancel_charge() - Cancel the charge from the misc cgroup.
@@ -371,6 +404,33 @@ static struct cftype misc_cg_files[] = {
 	{}
 };
 
+static inline int _misc_cg_res_alloc(struct misc_cg *cg)
+{
+	enum misc_res_type i;
+	int ret;
+
+	for (i = 0; i < MISC_CG_RES_TYPES; i++) {
+		WRITE_ONCE(cg->res[i].max, MAX_NUM);
+		atomic64_set(&cg->res[i].usage, 0);
+		if (misc_res_ops[i]) {
+			ret = misc_res_ops[i]->alloc(cg);
+			if (ret)
+				return ret;
+		}
+	}
+
+	return 0;
+}
+
+static inline void _misc_cg_res_free(struct misc_cg *cg)
+{
+	enum misc_res_type i;
+
+	for (i = 0; i < MISC_CG_RES_TYPES; i++)
+		if (misc_res_ops[i])
+			misc_res_ops[i]->free(cg);
+}
+
 /**
  * misc_cg_alloc() - Allocate misc cgroup.
  * @parent_css: Parent cgroup.
@@ -383,20 +443,25 @@ static struct cftype misc_cg_files[] = {
 static struct cgroup_subsys_state *
 misc_cg_alloc(struct cgroup_subsys_state *parent_css)
 {
-	enum misc_res_type i;
-	struct misc_cg *cg;
+	struct misc_cg *parent_cg, *cg;
+	int ret;
 
 	if (!parent_css) {
-		cg = &root_cg;
+		parent_cg = cg = &root_cg;
 	} else {
 		cg = kzalloc(sizeof(*cg), GFP_KERNEL);
 		if (!cg)
 			return ERR_PTR(-ENOMEM);
+		parent_cg = css_misc(parent_css);
 	}
 
-	for (i = 0; i < MISC_CG_RES_TYPES; i++) {
-		WRITE_ONCE(cg->res[i].max, MAX_NUM);
-		atomic64_set(&cg->res[i].usage, 0);
+	ret = _misc_cg_res_alloc(cg);
+	if (ret) {
+		_misc_cg_res_free(cg);
+		if (likely(parent_css))
+			kfree(cg);
+
+		return ERR_PTR(ret);
 	}
 
 	return &cg->css;
@@ -410,7 +475,10 @@ misc_cg_alloc(struct cgroup_subsys_state *parent_css)
  */
 static void misc_cg_free(struct cgroup_subsys_state *css)
 {
-	kfree(css_misc(css));
+	struct misc_cg *cg = css_misc(css);
+
+	_misc_cg_res_free(cg);
+	kfree(cg);
 }
 
 /* Cgroup controller callbacks */
