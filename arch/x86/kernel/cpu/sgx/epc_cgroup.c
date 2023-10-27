b49c4b739640f09d1b3ab0160ae5f50d7c007aac
Kristen Carlson Accardi <kristen@linux.intel.com>
2023-10-26 22:18:21 -0700

    x86/sgx: Implement basic EPC misc cgroup functionality
    SGX Enclave Page Cache (EPC) memory allocations are separate from normal
    RAM allocations, and are managed solely by the SGX subsystem. The
    existing cgroup memory controller cannot be used to limit or account for
    SGX EPC memory, which is a desirable feature in some environments.  For
    example, in a Kubernates environment, a user can request certain EPC
    quota for a pod but the orchestrator can not enforce the quota to limit
    runtime EPC usage of the pod without an EPC cgroup controller.
    
    Utilize the misc controller [admin-guide/cgroup-v2.rst, 5-9. Misc] to
    limit and track EPC allocations per cgroup. Earlier patches have added
    the "sgx_epc" resource type in the misc cgroup subsystem. Add basic
    support in SGX driver as the "sgx_epc" resource provider:
    
    - Set "capacity" of EPC by calling misc_cg_set_capacity()
    - Update EPC usage counter, "current", by calling charge and uncharge
    APIs for EPC allocation and deallocation, respectively.
    - Setup sgx_epc resource type specific callbacks, which perform
    initialization and cleanup during cgroup allocation and deallocation,
    respectively.
    
    With these changes, the misc cgroup controller enables user to set a hard
    limit for EPC usage in the "misc.max" interface file. It reports current
    usage in "misc.current", the total EPC memory available in
    "misc.capacity", and the number of times EPC usage reached the max limit
    in "misc.events".
    
    For now, the EPC cgroup simply blocks additional EPC allocation in
    sgx_alloc_epc_page() when the limit is reached. Reclaimable pages are
    still tracked in the global active list, only reclaimed by the global
    reclaimer when the total free page count is lower than a threshold.
    
    Later patches will reorganize the tracking and reclamation code in the
    global reclaimer and implement per-cgroup tracking and reclaiming.
    
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>
    Co-developed-by: Haitao Huang <haitao.huang@linux.intel.com>
    Signed-off-by: Haitao Huang <haitao.huang@linux.intel.com>
    ---
    V7:
    - Use a static for root cgroup (Kai)
    - Wrap epc_cg field in sgx_epc_page struct with #ifdef (Kai)
    - Correct check for charge API return (Kai)
    - Start initialization in SGX device driver init (Kai)
    - Remove unneeded BUG_ON (Kai)
    - Split  sgx_get_current_epc_cg() out of sgx_epc_cg_try_charge() (Kai)
    
    V6:
    - Split the original large patch"Limit process EPC usage with misc
    cgroup controller"  and restructure it (Kai)

---
+++ arch/x86/kernel/cpu/sgx/epc_cgroup.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright(c) 2022 Intel Corporation.
+
+#include <linux/atomic.h>
+#include <linux/kernel.h>
+#include "epc_cgroup.h"
+
+static struct sgx_epc_cgroup epc_cg_root;
+
+/**
+ * sgx_epc_cgroup_try_charge() - try to charge cgroup for a single EPC page
+ *
+ * @epc_cg:	The EPC cgroup to be charged for the page.
+ * Return:
+ * * %0 - If successfully charged.
+ * * -errno - for failures.
+ */
+int sgx_epc_cgroup_try_charge(struct sgx_epc_cgroup *epc_cg)
+{
+	if (!epc_cg)
+		return -EINVAL;
+
+	return  misc_cg_try_charge(MISC_CG_RES_SGX_EPC, epc_cg->cg, PAGE_SIZE);
+}
+
+/**
+ * sgx_epc_cgroup_uncharge() - uncharge a cgroup for an EPC page
+ * @epc_cg:	The charged epc cgroup
+ */
+void sgx_epc_cgroup_uncharge(struct sgx_epc_cgroup *epc_cg)
+{
+	if (!epc_cg)
+		return;
+
+	misc_cg_uncharge(MISC_CG_RES_SGX_EPC, epc_cg->cg, PAGE_SIZE);
+}
+
+static void sgx_epc_cgroup_free(struct misc_cg *cg)
+{
+	struct sgx_epc_cgroup *epc_cg;
+
+	epc_cg = sgx_epc_cgroup_from_misc_cg(cg);
+	if (!epc_cg)
+		return;
+
+	kfree(epc_cg);
+}
+
+static int sgx_epc_cgroup_alloc(struct misc_cg *cg);
+
+const struct misc_res_ops sgx_epc_cgroup_ops = {
+	.alloc = sgx_epc_cgroup_alloc,
+	.free = sgx_epc_cgroup_free,
+};
+
+static void sgx_epc_misc_init(struct misc_cg *cg, struct sgx_epc_cgroup *epc_cg)
+{
+	cg->res[MISC_CG_RES_SGX_EPC].priv = epc_cg;
+	epc_cg->cg = cg;
+}
+
+static int sgx_epc_cgroup_alloc(struct misc_cg *cg)
+{
+	struct sgx_epc_cgroup *epc_cg;
+
+	epc_cg = kzalloc(sizeof(*epc_cg), GFP_KERNEL);
+	if (!epc_cg)
+		return -ENOMEM;
+
+	sgx_epc_misc_init(cg, epc_cg);
+
+	return 0;
+}
+
+void sgx_epc_cgroup_init(void)
+{
+	misc_cg_set_ops(MISC_CG_RES_SGX_EPC, &sgx_epc_cgroup_ops);
+	sgx_epc_misc_init(misc_cg_root(), &epc_cg_root);
+}
